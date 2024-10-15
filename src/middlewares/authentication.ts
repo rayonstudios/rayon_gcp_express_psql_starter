ecf934708e578b3d801eea1eeee7c53107e45003
MohammedMaaz <maazkhawar25@gmail.com>
2024-10-15 17:48:05 +0500

    saved jwt user data in res.locals.user

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { toResponse } from "../lib/utils";
 import { errorConst } from "../lib/utils/error";
 import authService from "../modules/auth/auth.service";
+import { AuthUser } from "../modules/auth/auth.types";
 
 export const validateAuthentication = async (
   req: Request,
@@ -10,12 +11,18 @@ export const validateAuthentication = async (
 ) => {
   const token = req.headers.authorization?.split(" ")[1];
   const type = req.originalUrl.endsWith("/refresh") ? "refresh" : "access";
-  if (!token || !(await authService.verifyToken(token, type))) {
-    return res.status(errorConst.unAuthenticated.code).json(
+  const sendUnAuthResponse = () =>
+    res.status(errorConst.unAuthenticated.code).json(
       toResponse({
         error: errorConst.unAuthenticated.message,
       })
     );
-  }
+
+  if (!token) return sendUnAuthResponse();
+
+  const user = await authService.verifyToken(token, type);
+  if (!user) return sendUnAuthResponse();
+
+  res.locals.user = user as AuthUser;
   next();
 };
