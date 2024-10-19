fdd3efba5ec8d079c842ff3593d2017b845956cb
MuhammadUsama-hub <muhammadusama798i@gmail.com>
2024-10-19 15:08:13 +0500

    just created

---
+++ src/lib/mail/mail.types.ts
@@ -0,0 +1,5 @@
+import sibTemplates from "../mail/mail.templates";
+export type AuthTemplateType = keyof typeof sibTemplates.authentication;
+export type AuthTemplateParams = Parameters<
+  (typeof sibTemplates.authentication)[AuthTemplateType]
+>[0];
