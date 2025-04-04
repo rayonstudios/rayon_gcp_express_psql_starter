edf164e8990cc920976db0c4e05784aa373d5852
MohammedMaaz <maazkhawar25@gmail.com>
2025-04-04 13:26:26 +0500

    feat: refactor API endpoints into MiscController for better organization and add security to reload secrets

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 import { RegisterRoutes } from "#/routes";
-import { fetchSecrets } from "#/scripts/helpers";
 import cors from "cors";
 import express, { json, urlencoded } from "express";
 import morgan from "morgan";
 import multer from "multer";
 import swaggerUi from "swagger-ui-express";
-import { isDevEnv, toResponse } from "./lib/utils";
-import { statusConst } from "./lib/utils/status";
 import { globalErrorHandler } from "./middlewares/error.middleware";
 import { setupSwagger } from "./middlewares/swagger.middleware";
 
@@ -33,37 +30,6 @@ RegisterRoutes(app, {
   multer: multer({
     dest: "/tmp",
   }),
-});
-
-// open api
-app.use("/api/openapi.json", (_, res) => {
-  res.sendFile("swagger.json", { root: "." });
-});
-
-// status check
-app.get("/api/status", (_, res) => {
-  res.status(200).send(
-    toResponse({
-      data: { message: `Hello World! This is ${process.env.NODE_ENV} env` },
-    })
-  );
-});
-
-// reload secrets webhook
-app.post("/api/reload_secrets", async (req, res) => {
-  const { key } = req.query;
-  if (key !== process.env.INFISICAL_WEBHOOK_KEY) {
-    res
-      .status(statusConst.unAuthenticated.code)
-      .json(toResponse({ error: statusConst.unAuthenticated.message }));
-    return;
-  }
-
-  const secrets = await fetchSecrets(isDevEnv() ? "dev" : "production");
-  secrets.forEach((secret) => {
-    process.env[secret.secretKey] = secret.secretValue;
-  });
-  res.status(200).json(toResponse({ data: { message: "Secrets reloaded" } }));
 });
 
 // swagger docs
---
+++ src/modules/misc/misc.controller.ts
@@ -0,0 +1,37 @@
+import { fetchSecrets } from "#/scripts/helpers";
+import { APIResponse, Message } from "#/src/lib/types/misc";
+import { GenericObject } from "#/src/lib/types/utils";
+import { isDevEnv, toResponse } from "#/src/lib/utils";
+import { Controller, Get, Post, Query, Route, Security, Tags } from "tsoa";
+
+@Route("")
+@Tags("Miscellaneous")
+export class MiscController extends Controller {
+  @Get("/status")
+  public async status(): Promise<APIResponse<Message>> {
+    return toResponse({
+      data: { message: `Hello World! This is ${process.env.NODE_ENV} env` },
+    });
+  }
+
+  @Get("/openapi.json")
+  public async openApi(): Promise<GenericObject> {
+    const json = await import("#/swagger.json");
+    return json;
+  }
+
+  @Post("/reload-secrets")
+  @Security("api_key")
+  public async reloadSecrets(
+    @Query() api_key: string
+  ): Promise<APIResponse<Message>> {
+    const secrets = await fetchSecrets(isDevEnv() ? "dev" : "production");
+    secrets.forEach((secret) => {
+      process.env[secret.secretKey] = secret.secretValue;
+    });
+
+    return toResponse({
+      data: { message: "Secrets reloaded" },
+    });
+  }
+}
